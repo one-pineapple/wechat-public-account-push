c35a63f700114870c9608b857ef791e54cf6563d
one-pineapple <109949468+one-pineapple@users.noreply.github.com>
2024-01-19 13:41:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '10-13',
+          type: '*生日', name: '小师傅', year: '1996', date: '10-13',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '11-23',
+          type: '生日', name: '小师傅', year: '1996', date: '11-23',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '09-16',
