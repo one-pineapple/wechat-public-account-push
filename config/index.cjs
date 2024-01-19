60397ca5f392cc6dfd426e5ebed4220be06feffa
one-pineapple <109949468+one-pineapple@users.noreply.github.com>
2024-01-19 13:27:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oYhcb6iUokacVRpOLDRu2xV7cIGY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Vs2kjj834IoFaIKK-sHeUJlIxVFZx0-ObfeIIQ8cil8',
+      useTemplateId: '8Wc4rfeR9vm-pHUeDl7AoVr6-oUUuGDCqYGj_V2a7Vo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-23',
       festivals: [
